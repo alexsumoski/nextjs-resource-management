b022f76dc264cecbf7e2c5028b736c4291763a7e
Alex Sumoski <alexsumoski@gmail.com>
2022-08-13 22:09:01 -0400

    state recap

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
 
 export default function Home() {
+  const [myValue, setValue] = useState(0);
+
   return (
     <div className={styles.container}>
-      
+      <span>Current Value: { myValue }</span>
+      <button onClick={() => setValue(myValue+1)}>+</button>
+      <button onClick={() => setValue(myValue-1)}>-</button>
     </div>
   )
 }
